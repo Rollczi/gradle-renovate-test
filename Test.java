d6724fcabb750cb57252ebf872de701451a2da04
Norbert Dejlich <ndejlich5@gmail.com>
2026-02-19 20:35:56 +0100

    Add Redis and PostgreSQL containers to Kafka tests
    Added Redis and PostgreSQL containers for testing.

--- Test.java
+++ Test.java
@@ -5,6 +5,15 @@
 @SpringBootTest
 @Testcontainers
 public class KafkaContainerTest {
+
+    private static final GenericContainer<?> redis = new GenericContainer<>("redis:7.0.5")
+        .withExposedPorts(6379);
+
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer("postgres:14.1") 
+        .withDatabaseName("testdb");
+
+    private static final DockerImageName kafkaImage = DockerImageName.parse("confluentinc/cp-kafka:7.2.1"); 
+    
     @ClassRule
     public static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.2.1"));
 
